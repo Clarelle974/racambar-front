6ee896ede71da244675e94defd6ccb453c6fefb3
Clarelle974 <clarelle.lemonnier@gmail.com>
2025-06-24 00:22:50 +0200

    remove / in API live URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL.replace(/\/+$/, '');
 
 type Joke = {
 	id: number;
